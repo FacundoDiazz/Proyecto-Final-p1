1429400b4a2c77e0bd00e231b2b8b11048658c81
FacundoDiazz <facudiaz1891@gmail.com>
2024-08-09 19:35:44 -0300

    ultimo repo

---
+++ archivo.js
@@ -0,0 +1,7 @@
+
+
+window.addEventListener("load", function() {
+    document.getElementById("alert").addEventListener("click", function() {
+        alert("Usted a sidp registrado con exito");
+    })
+})
