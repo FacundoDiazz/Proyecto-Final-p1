bb5bd51bcb87d72e86c8b9d61beeac161d3fe9fa
FacundoDiazz <facudiaz1891@gmail.com>
2024-10-23 19:19:08 -0300

    parte 2

--- archivo.js
+++
@@ -1,7 +0,0 @@
-
-
-window.addEventListener("load", function() {
-    document.getElementById("alert").addEventListener("click", function() {
-        alert("Usted a sidp registrado con exito");
-    })
-})
